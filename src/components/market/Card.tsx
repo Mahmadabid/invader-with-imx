e2be5c4f8d9f3aafec71afbf4c0436a3b2416216
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2024-01-10 21:06:50 +0500

    Update Card.tsx

--- src/components/market/Card.tsx
+++ src/components/market/Card.tsx
@@ -5,9 +5,9 @@ const Card: React.FC<{ image: string; name: string; price: string; onButtonClick
     onButtonClick,
 }) => {
     return (
-        <div className="max-w-xs mx-auto bg-white shadow-lg shadow-slate-600 rounded-lg overflow-hidden mb-4">
+        <div className="max-w-xs mx-auto bg-white mx-6 shadow-lg shadow-slate-600 rounded-lg overflow-hidden mb-8">
             {image === '/gray.png' ?
-                <div className="bg-gray-900 text-white w-56 h-96 flex items-center justify-center">
+                <div className="bg-gray-900 text-white w-56 h-[389px] flex items-center justify-center">
                     <p className="text-xl font-bold">Coming Soon</p>
                 </div>
                 :
@@ -15,9 +15,9 @@ const Card: React.FC<{ image: string; name: string; price: string; onButtonClick
                     <img className="w-56 h-56 object-cover object-center" src={image} alt={name} />
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">{name}</div>
-                        <p className="text-gray-700 text-base font-medium flex flex-row">{price} <img src="/IPX.png" width={28} height={28} /></p>
+                        <p className="text-gray-700 text-base font-medium flex flex-row">{price}&nbsp; <img src="/IPX.png" width={25} height={25} /></p>
                     </div>
-                    <div className="px-6 pt-4">
+                    <div className="px-6 py-4">
                         <button
                             onClick={onButtonClick}
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
