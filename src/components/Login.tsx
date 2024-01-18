4023cf3dc77bc63714f36dd6c748d39efaa1008b
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2024-01-18 23:50:13 +0500

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -57,7 +57,7 @@ function Login({ userLoading }: LoadProps) {
   }, [connect]);
 
   const headerHeight = 4.6;
-
+console.log(loading, userLoading)
   return (
     <div className="flex items-center bg-gray-950 text-white justify-center" style={{ minHeight: `calc(100vh - ${headerHeight}rem)` }}>
       <div className="text-center">
@@ -65,7 +65,7 @@ function Login({ userLoading }: LoadProps) {
         {!Open ? <button onClick={() => {
           setOpen(true)
         }} className="font-bold text-xl bg-purple-500 text-white px-4 py-2 rounded-full hover:bg-purple-600 transition duration-300" disabled={loading || userLoading}>{loading || userLoading ? <Load /> : 'Log in with Wallet'}</button> : null}
-        <h1 className="text-2xl font-bold my-4">Reload in case of error or the button is still loading even after half a minute.</h1>
+        <h1 className="text-lg font-semibold my-4">Reload in case of error or the button is still loading even after half a minute.</h1>
         <div className='my-6' id="connect" />
       </div>
     </div>
